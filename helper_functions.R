906a302b12726aaab65058a0fc5ec7a69ca58387
Jake Riley <rileyj3@email.chop.edu>
2019-07-29 17:02:15 -0400

    Break out helper functions

---
+++ helper_functions.R
@@ -0,0 +1,135 @@
+# Get code ready for evaluation ----
+code_to_df <- function() {
+  clean_code <-
+    read_file(file_to_parse) %>%
+    str_replace_all(" +", " ") %>%
+    str_replace_all("\\n(\\s?)(#.*)\r", "") %>% # single line comments
+    str_replace_all("(\\%\\>\\%)( #[^\r]*)\r", "\\1 ") %>% # inline comments after pipe
+    str_replace_all("([,\\{\\(])( #[^\r]*)\r", "\\1 ") %>% # inline comments after brackets
+    str_replace_all("(\\<\\-)[ \r\n]+", "\\1 ") %>% # assignments
+    str_replace_all("(,)[ \r\n]+", "\\1 ") %>% # commas
+    str_replace_all("(\\%\\>\\%)[ \r\n]+", "\\1 ") %>% # dplyr pipe
+    str_replace_all("(\\+)[ \r\n]+", "\\1 ") %>% # ggplot pipe
+    str_replace_all("([\\{\\(]+)[ \r\n]+", "\\1 ") %>% # open bracket/parens
+    str_replace_all("[ \r\n]+([\\}\\)]+)", "\\1 ") # move close bracket/parens up
+  
+  # find closing parenteses for all open parenteses
+  n_open <- 0
+  final_edit <- ""
+  
+  # add ; to lines
+  for (i in 1:nchar(clean_code)) {
+    x <- str_sub(clean_code, i, i)
+    if (x == "{") {
+      n_open <- n_open + 1
+    }
+    if (x == "}") {
+      n_open <- n_open - 1
+    }
+    if (n_open > 0 && str_detect(x, "[\r\n]")) {
+      x <- ";"
+    }
+    final_edit <- paste0(final_edit, x)
+  }
+  
+  # turn code into df
+  tibble(text = read_lines(str_replace_all(final_edit, ";+", ";")))
+  
+}
+
+
+# Find input demo ----
+find_input_code <- function() {
+  # final_code <- code_to_df()
+  final_code %>%
+  filter(str_detect(text, "input <-")) %>%
+  pull(text)
+}
+
+
+# Write message based on inputs available ----
+validate_inputs <- function() {
+  
+  input_demo_values <-
+    find_input_code() %>%
+    str_extract_all("(\\w+)(?=\\s\\=)") %>%
+    unlist()
+  
+  input_ref <-
+    read_lines(file = file_to_parse) %>%
+    tibble(text = trimws(.)) %>%
+    mutate(
+      line = row_number(),
+      text = str_remove(text, "#.*") # remove comments
+    ) %>%
+    filter(str_detect(text, "input\\$\\w+")) %>%
+    mutate(input_name = str_extract_all(text, "input\\$\\w+")) %>%
+    unnest(input_name) %>%
+    distinct(input_name, line) %>%
+    group_by(input_name = str_remove(input_name, "input\\$")) %>%
+    summarise(
+      times_used = n(),
+      lines = glue_collapse(line, ",")
+    ) %>%
+    ungroup() %>%
+    mutate(missing = (!input_name %in% input_demo_values | length(input_demo_values) == 0))
+  
+  
+  input_ref
+  
+  
+  if (nrow(input_ref) == 0) { # no inputs
+    print("No inputs")
+  } else if (sum(input_ref$missing) > 0) { # missing references
+    input_df <-
+      input_ref %>%
+      filter(missing == TRUE)
+    
+    input_add <-
+      glue('{input_df$input_name} = ""') %>%
+      glue_collapse(sep = ", \n")
+    
+    rm(input_df)
+    
+    if (length(find_input_code()) == 0) { # no input demo, create new list
+      update_input_code <- glue("input <- list({input_add})")
+      
+      message("\n# Add this code chunk to your Rmd:\n")
+      message("```{r input_demo, eval = FALSE}")
+      print(styler::style_text(update_input_code))
+      message("```")
+      
+    } else { # append list
+      message("Update code:")
+      update_input_code <- glue("input <- list(\n..., \n{input_add}\n)")
+      # str_replace(trimws(input_demo), "\\)$", glue("\n, {input_add})"))
+      styler::style_text(update_input_code)
+    }
+    # print input statement
+  } else {
+    print("All inputs accounted for :)")
+  }
+}
+
+
+# Find reactive functions ----
+find_reactive_functions <- function() {
+  final_code %>%
+  filter(str_detect(text, "<- reactive")) %>%
+  #slice(21) %>%
+  mutate(
+    text =
+      trimws(text) %>%
+      str_replace("reactive\\((\\{)?", "function\\(\\)\\{") %>%
+      str_replace("\\}( )?\\)", "\\}") %>%
+      str_replace("\\)$", "\\}") %>%
+      str_replace_all('\"', "'")
+  ) %>%
+  pull(text)
+}
+
+# x <-
+#   tibble(text = c("a <- function(){ df <- get_data}", "b <- function(){ df <- get_data}")) %>%
+#   pull(text)
+#
+# eval(parse(text = x))
--- load_inputs_and_objects.R
+++ load_inputs_and_objects.R
@@ -3,166 +3,29 @@
 library(tidyverse)
 library(glue)
 
-selected_file <- file.choose()
+source(paste0(dirname(rstudioapi::getSourceEditorContext()$path), "/helper_functions.R"))
 
-# READ CODE ----
-# get code ready for evaluation
-make_edits <-
-  read_file(selected_file) %>%
-  str_replace_all(" +", " ") %>%
-  str_replace_all("\\n(\\s?)(#.*)\r", "") %>% # single line comments
-  str_replace_all("(\\%\\>\\%)( #[^\r]*)\r", "\\1 ") %>% # inline comments after pipe
-  str_replace_all("([,\\{\\(])( #[^\r]*)\r", "\\1 ") %>% # inline comments after brackets
-  str_replace_all("(\\<\\-)[ \r\n]+", "\\1 ") %>% # assignments
-  str_replace_all("(,)[ \r\n]+", "\\1 ") %>% # commas
-  str_replace_all("(\\%\\>\\%)[ \r\n]+", "\\1 ") %>% # dplyr pipe
-  str_replace_all("(\\+)[ \r\n]+", "\\1 ") %>% # ggplot pipe
-  str_replace_all("([\\{\\(]+)[ \r\n]+", "\\1 ") %>% # open bracket/parens
-  str_replace_all("[ \r\n]+([\\}\\)]+)", "\\1 ") # move close bracket/parens up
+file_to_parse <- file.choose()
 
-# find closing parenteses for all open parenteses
-n_open <- 0
-final_edit <- ""
-
-# add ; to lines
-for (i in 1:nchar(make_edits)) {
-  x <- str_sub(make_edits, i, i)
-  if (x == "{") {
-    n_open <- n_open + 1
-  }
-  if (x == "}") {
-    n_open <- n_open - 1
-  }
-  if (n_open > 0 && str_detect(x, "[\r\n]")) {
-    x <- ";"
-  }
-  final_edit <- paste0(final_edit, x)
-}
-
-# turn code into df
-final_code <-
-  tibble(text = read_lines(str_replace_all(final_edit, ";+", ";")))
-  
+final_code <- code_to_df()
 
 # INPUTS ----
 # make sure demo inputs exist (if required)
-input_demo <-
-  final_code %>%
-  filter(str_detect(text, "input <-")) %>%
-  pull(text)
-
-
-input_demo_values <-
-  input_demo %>%
-  str_extract_all("(\\w+)(?=\\s\\=)") %>%
-  unlist()
-
-
-validate_inputs <- function() {
-  
-  input_ref <-
-    read_lines(file = selected_file) %>%
-    tibble(text = trimws(.)) %>%
-    mutate(
-      line = row_number(),
-      text = str_remove(text, "#.*") # remove comments
-    ) %>%
-    filter(str_detect(text, "input\\$\\w+")) %>%
-    mutate(input_name = str_extract_all(text, "input\\$\\w+")) %>%
-    unnest(input_name) %>%
-    distinct(input_name, line) %>%
-    group_by(input_name = str_remove(input_name, "input\\$")) %>%
-    summarise(
-      times_used = n(),
-      lines = glue_collapse(line, ",")
-    ) %>%
-    ungroup() %>%
-    mutate(missing = (!input_name %in% input_demo_values | length(input_demo_values) == 0))
-
-
-  input_ref
-
-
-  if (nrow(input_ref) == 0) { # no inputs
-    print("No inputs")
-  } else if (sum(input_ref$missing) > 0) { # missing references
-    input_df <-
-      input_ref %>%
-      filter(missing == TRUE)
-
-    input_add <-
-      glue('{input_df$input_name} = ""') %>%
-      glue_collapse(sep = ", \n")
-
-    rm(input_df)
-
-    if (length(input_demo) == 0) { # no input demo, create new list
-      update_input_code <- glue("input <- list({input_add})")
-      message(
-        glue(
-          "# Add this code chunk to your Rmd:
-        \n```{{r input_demo, eval = FALSE}}\n\n",
-          styler::style_text(update_input_code),
-          "\n\n```"
-        )
-      )
-    } else { # append list
-      message("Update code:")
-      update_input_code <- glue("input <- list(\n..., \n{input_add}\n)")
-      # str_replace(trimws(input_demo), "\\)$", glue("\n, {input_add})"))
-      styler::style_text(update_input_code)
-    }
-    # print input statement
-  } else {
-    print("All inputs accounted for :)")
-  }
-}
-
 validate_inputs()
 
 # * load inputs ----
-eval(parse(text = input_demo))
+eval(parse(text = find_input_code()))
 
 
 # REACTIVE FUNCTIONS ----
-reactive_functions <-
-  final_code %>%
-  filter(str_detect(text, "<- reactive")) %>%
-  #slice(21) %>%
-  mutate(
-    text =
-      trimws(text) %>%
-      str_replace("reactive\\((\\{)?", "function\\(\\)\\{") %>%
-      str_replace("\\}( )?\\)", "\\}") %>%
-      str_replace("\\)$", "\\}") %>%
-      str_replace_all('\"', "'")
-  ) %>%
-  pull(text)
-
-
 # * load functions ----
-eval(parse(text = reactive_functions))
-
-
-# x <-
-#   tibble(text = c("a <- function(){ df <- get_data}", "b <- function(){ df <- get_data}")) %>%
-#   pull(text)
-#
-# eval(parse(text = x))
+eval(parse(text = find_reactive_functions()))
 
 rm(
+  code_to_df,
+  file_to_parse,
   final_code,
-  #input,
-  input_ref,
-  final_edit,
-  i,
-  input_add,
-  input_demo,
-  input_demo_values,
-  make_edits,
-  n_open,
-  reactive_functions,
-  selected_file,
-  update_input_code,
-  x
+  find_input_code,
+  find_reactive_functions,
+  validate_inputs
 )
