2512d767cb4d71683716d7bb6cc0ec19e18cd36e
rjake <rjake@sas.upenn.edu>
2019-08-22 00:42:20 -0400

    Create for_server_files.R

---
+++ R/for_server_files.R
@@ -0,0 +1,60 @@
+char_between <- function(text, pattern = c("c", "p")) {
+  pattern <- match.arg(pattern)
+  regex_pattern <-
+    case_when(
+      pattern == "c" ~ "[\\{\\}]",
+      pattern == "p" ~ "[\\(\\)]",
+      TRUE ~ NA_character_ 
+    )
+  
+  if (is.na(regex_pattern)) {
+    stop('"c" curly braces {} or "p" parentheses () not specified') 
+  }
+  
+  pattern_match <-  
+    str_locate_all(string = as.character(text), pattern = regex_pattern)[[1]] %>% 
+    range()
+  
+  substr(text, pattern_match[1] + 1, pattern_match[2] - 1) %>% 
+    trimws()
+  
+}
+
+
+
+
+parse_server_file <- function(file) {
+  output <- list()
+  
+  raw_code <- parse(file)
+  
+  server_line <- grep("^(shiny)?server\\b", raw_code, ignore.case = TRUE)
+  
+  if (length(server_line) == 0) {stop("server not found")}
+  if (length(server_line) > 1) {warning("more than one server assignment found")}
+  
+  server_code <- raw_code[server_line]
+  
+  new_code <-
+    char_between(server_code) %>% 
+    convert_assignments() %>% 
+    parse(text = .)
+  
+  to_eval <- expression()
+  
+  if (server_line == 1) {
+    to_eval <- c(to_eval, new_code)
+  } else {
+    to_eval <- c(raw_code[1:(server_line-1)], new_code)
+  }
+  
+  if (server_line != length(raw_code)) {
+    to_eval <- c(to_eval, raw_code[(server_line + 1):length(raw_code)])
+  }
+  
+  to_eval
+}
+
+input_usage(file = "inst/shiny/server.R")
+require(shiny)
+parse_server_file(file = "inst/shiny/server.R") %>% eval()
