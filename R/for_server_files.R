88f632523d98cd8f3157c8e0f994d01695590266
jake <rjake@sas.upenn.edu>
2019-08-25 19:03:17 -0400

    Allow function to run if server <- is missing

--- R/for_server_files.R
+++ R/for_server_files.R
@@ -47,14 +47,24 @@ char_between <- function(text, pattern = c("c", "p")) {
 #'   eval(envir = .GlobalEnv)
 #' }
 parse_server_file <- function(file) {
-  output <- list()
+  output <<- list()
   
   raw_code <- parse(file)
   
   server_line <- grep("^(shiny)?server\\b", raw_code, ignore.case = TRUE)
   
-  if (length(server_line) == 0) {stop("server not found")}
-  if (length(server_line) > 1) {warning("more than one server assignment found")}
+  server_missing <- length(server_line) == 0
+  
+  if (length(server_line) > 1) {stop("more than one server assignment found")}
+  
+  if (server_missing) {
+    warning("server not found, using whole file")
+    new_code <-
+      raw_code %>% 
+      convert_assignments() %>% 
+      parse(text = .)
+  }
+  
   
   server_code <- raw_code[server_line]
   
@@ -65,19 +75,17 @@ parse_server_file <- function(file) {
   
   to_eval <- expression()
   
-  if (server_line == 1) {
+  # if the first line is "server <- " or server is not specified
+  if (server_line == 1 | server_missing) {
     to_eval <- c(to_eval, new_code)
-  } else {
-    to_eval <- c(raw_code[1:(server_line-1)], new_code)
+  } else {# replace the server <- with the assignments contained within
+    to_eval <- c(raw_code[1:(server_line - 1)], new_code)
   }
   
+  # if there is extra code after "server <-" (server assignment isn't the last assignment)
   if (server_line != length(raw_code)) {
     to_eval <- c(to_eval, raw_code[(server_line + 1):length(raw_code)])
   }
   
   to_eval
 }
-
-input_usage(file = "inst/shiny/server.R")
-require(shiny)
-parse_server_file(file = "inst/shiny/server.R") %>% eval()
