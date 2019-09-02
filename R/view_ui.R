0c1f9107b6b2c6f6fa95ac67f682128d753df1d5
rjake <rjake@sas.upenn.edu>
2019-09-02 12:11:19 -0400

    Add stopApp to imports

--- R/view_ui.R
+++ R/view_ui.R
@@ -2,7 +2,7 @@
 #'
 #' @param x ui content (actionButton, selectInput, valueBox), defaults to last output, expects an object with class "shiny.tag"
 #' @param close_after number of seconds to display UI in Viewer panel. If NULL, app must be stopped manually before more code can be run.
-#' @importFrom shiny shinyApp fluidPage runApp
+#' @importFrom shiny shinyApp fluidPage runApp stopApp
 #' @importFrom rstudioapi viewer
 #' @examples 
 #' \dontrun{
