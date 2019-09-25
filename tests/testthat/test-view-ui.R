39711f457f31b15374c963920bd287ff0625bf29
rjake <rjake@sas.upenn.edu>
2019-09-24 22:21:56 -0400

    Add test for error

--- tests/testthat/test-view-ui.R
+++ tests/testthat/test-view-ui.R
@@ -1,12 +1,19 @@
 library(shiny)
 
+test_that("error if not shiny.tag", {
+  x <- ""
+  
+  expect_error(view_ui(x))
+})
+
+
 test_that("shiny.tag.list is accepted", {
   x <- 
     tagList(
       h4("A header"),
       selectInput("select", "Select here", choices = 1:10)
-    ) 
-  
+    )
+
   expect_message(
     view_ui(x, close_after = 0.1),
     regexp = "Listening"
