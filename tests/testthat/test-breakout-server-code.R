cc7277b19b90e4fafc595c365a39a165799fe26a
rjake <rjake@sas.upenn.edu>
2019-10-12 01:08:25 -0400

    Remove expect_warning

--- tests/testthat/test-breakout-server-code.R
+++ tests/testthat/test-breakout-server-code.R
@@ -7,14 +7,13 @@ test_that("stop if multiple server <- ", {
 })
 
 
-test_that("warning if missing server <- ", {
+test_that("use everything when missing server <- ", {
   code <- "a <- 1; b <- 2;"
   expected <- c("a <- 1", "b <- 2")
 
   tmp <- tempfile("data")
   write(code, tmp)
-  expect_warning(breakout_server_code(file = tmp)) 
-  actual <- suppressWarnings(breakout_server_code(file = tmp))
+  actual <- breakout_server_code(file = tmp)
   unlink(tmp)
   
   expect_equal(expected, actual)
