24440e2a1d17228515aeb7181d04101e3b104a51
rjake <rjake@sas.upenn.edu>
2019-10-12 01:00:09 -0400

    Use code from master

--- R/for_server_files.R
+++ R/for_server_files.R
@@ -56,40 +56,26 @@ breakout_server_code <- function(file) {
   raw_code <- as.character(parse(file))
   
   server_line <- grep("^(shiny)?server\\b", raw_code, ignore.case = TRUE)
+  app_line <- grep("^(shiny|run)App\\(.*server =", raw_code, ignore.case = TRUE)
+  has_server_line <- length(server_line) > 0
+  has_app_line <- length(app_line) > 0
   
-  server_missing <- length(server_line) == 0
   
-  if (length(server_line) > 1) {
-    stop("more than one server assignment found", call. = FALSE)
-  }
+  if (length(server_line) > 1) {stop("more than one server assignment found")}
   
-  to_eval <- vector("character", 0)
-  
-  if (server_missing) { # treat all code as "server" code
-    warning("server not found, using whole file", call. = FALSE)
-    new_code <- unlist(lapply(raw_code, convert_assignments))
-    to_eval <- new_code
-  } else {
+  if (!has_server_line & !has_app_line) { # use the code as is
+    final_code <- raw_code
+    
+  } else if (has_server_line) { # convert the "server <-" line
     server_code <- raw_code[server_line]
+    new_code <- char_between(server_code)
+    final_code <- append(raw_code, new_code, after = server_line)[-server_line]
     
-    new_code <-
-      unlist(
-        lapply(char_between(server_code, pattern = "c"), convert_assignments)
-      )
-    
-    if (server_line == 1) { # use just the server code
-      to_eval <- new_code
-    } else {# replace the server <- with the assignments contained within
-      to_eval <- c(raw_code[1:(server_line - 1)], new_code)
-    }
-    
-    # if there is extra code after "server <-" (server assignment isn't the last assignment)
-    if(server_line != length(raw_code)) {
-      to_eval <- c(to_eval, raw_code[(server_line + 1):length(raw_code)])
-    }
-    
+  } else if (has_app_line) { # extract out of "runApp()" or "shinyApp()"
+    server_code <- raw_code[app_line]
+    new_code <- extract_from_app_fn(server_code)
+    final_code <- append(raw_code, new_code, after = app_line)[-app_line]
   }
   
-  
-  to_eval
+  to_eval <- convert_assignments(final_code)
 }
