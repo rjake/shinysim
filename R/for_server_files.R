0df7f0a1daa8a95ed29f654bec4e1f3f4450eb82
rjake <rjake@sas.upenn.edu>
2019-10-12 01:04:29 -0400

    Shiffle code logic

--- R/for_server_files.R
+++ R/for_server_files.R
@@ -52,7 +52,6 @@ char_between <- function(text, pattern = c("c", "p")) {
 #'   eval(envir = .GlobalEnv)
 #' }
 breakout_server_code <- function(file) {
-  
   raw_code <- as.character(parse(file))
   
   server_line <- grep("^(shiny)?server\\b", raw_code, ignore.case = TRUE)
@@ -61,21 +60,25 @@ breakout_server_code <- function(file) {
   has_app_line <- length(app_line) > 0
   
   
-  if (length(server_line) > 1) {stop("more than one server assignment found")}
-  
-  if (!has_server_line & !has_app_line) { # use the code as is
-    final_code <- raw_code
-    
-  } else if (has_server_line) { # convert the "server <-" line
-    server_code <- raw_code[server_line]
-    new_code <- char_between(server_code)
-    final_code <- append(raw_code, new_code, after = server_line)[-server_line]
-    
-  } else if (has_app_line) { # extract out of "runApp()" or "shinyApp()"
-    server_code <- raw_code[app_line]
-    new_code <- extract_from_app_fn(server_code)
-    final_code <- append(raw_code, new_code, after = app_line)[-app_line]
+  if (length(server_line) > 1) {
+    stop("more than one server assignment found", call. = FALSE)
   }
   
-  to_eval <- convert_assignments(final_code)
+  if (!has_server_line & !has_app_line) { # treat all code as "server" code
+    final_code <- raw_code
+  } else {
+    server_code <- raw_code[server_line]
+    if (has_app_line) { # extract out of "runApp()" or "shinyApp()"
+      server_code <- raw_code[app_line]
+      new_code <- extract_from_app_fn(server_code)
+      final_code <- append(raw_code, new_code, after = app_line)[-app_line]
+    } else if (has_server_line) { # convert the "server <-" line
+      server_code <- raw_code[server_line]
+      new_code <- char_between(server_code, pattern = "c")
+      final_code <- append(raw_code, new_code, after = server_line)[-server_line]
+    }
+    
+  }
+  
+  to_eval <- unlist(lapply(final_code, convert_assignments))
 }
