cc15cbc5e375a6201dcf8d32227011898b80b3c4
rjake <rjake@sas.upenn.edu>
2019-11-05 20:36:49 -0500

    Update documentation

--- R/source_editor_context.R
+++ R/source_editor_context.R
@@ -1,6 +1,6 @@
 # nocov start
 
-#' Convert assignment from reactive to a function
+#' Convert and load the hightlighted assignment to the global environment
 #'
 #' After highlighting the assignment in the source editor, go to 
 #' the console and run this function. The selected code will be run 
